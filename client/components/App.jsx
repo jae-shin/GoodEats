235ba4637b7f16113bdcce53ceb010fc0f69bae1
Jae Shin <jae5464@gmail.com>
2016-07-11 16:32:10 -0700

    Add client components directory

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import { render } from 'react-dom'
+// import { Router, Route, Link, browserHistory } from 'react-router'
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Link = ReactRouter.Link;
+
+var App = () => (
+  <div>
+    <h3>Good Eats</h3>
+    <ul>
+      <li><Link to="/places">Places</Link></li>
+      <li><Link to="/add">Add</Link></li>
+    </ul>
+  </div>
+);
