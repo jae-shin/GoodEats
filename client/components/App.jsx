5e3d5987365ead498f6f362eedc77e26c4cdae3b
Jae Shin <jae5464@gmail.com>
2016-07-11 18:51:13 -0700

    Fix compile command and render static index html through express server

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,19 @@
 // import React from 'react'
 // import { render } from 'react-dom'
 // import { Router, Route, Link, browserHistory } from 'react-router'
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-var Link = ReactRouter.Link;
+// var Router = ReactRouter.Router;
+// var Route = ReactRouter.Route;
+// var Link = ReactRouter.Link;
 
-var App = () => (
+var App = (props) => (
   <div>
-    <h3>Good Eats</h3>
-    <ul>
-      <li><Link to="/places">Places</Link></li>
-      <li><Link to="/add">Add</Link></li>
-    </ul>
+    <h3>{props.title}</h3>
   </div>
 );
 
 window.App = App;
+
+// <ul>
+//   <li><Link to="/places">Places</Link></li>
+//   <li><Link to="/add">Add</Link></li>
+// </ul>
--- client/index.js
+++ client/index.js
@@ -1,7 +1,4 @@
 // import ReactDOM from 'react-dom'
 // import App from './components/App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App title={'Good Eats'}/>, document.getElementById('app'));
