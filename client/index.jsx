82d94c03995d289e5be007ccd9aab2dc70c1f3c9
Jae Shin <jae5464@gmail.com>
2016-08-28 22:28:37 -0700

    Change file extension of index file from .js to .jsx

---
+++ client/index.jsx
@@ -0,0 +1,19 @@
+window.Router = window.ReactRouter.Router;
+window.Route = window.ReactRouter.Route;
+window.Link = window.ReactRouter.Link;
+window.browserHistory = window.ReactRouter.browserHistory;
+window.IndexRoute = window.ReactRouter.IndexRoute;
+
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={PlaceList}/>
+      <Route path="places" component={PlaceList}/>
+      <Route path="create" component={CreateForm}/>
+      <Route path="signup" component={Signup}/>
+      <Route path="signin" component={Signin}/>
+    </Route>
+  </Router>
+), document.getElementById('app'));
+
